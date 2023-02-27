9cdbb09a905425594bb372a014dbbc5cd944c222
hatien <hatien@tma.com.vn>
2023-02-27 17:16:24 +0700

    update

--- src/main/java/multithread/CachedThreadPoolExample.java
+++ src/main/java/multithread/CachedThreadPoolExample.java
@@ -6,9 +6,8 @@ import java.util.concurrent.Executors;
 public class CachedThreadPoolExample {
     public static void main(String[] args) throws InterruptedException {
         ExecutorService executor = Executors.newCachedThreadPool();
-
+         
         // Có 100 request tới cùng lúc
-
         for (int i = 0; i < 100; i++) {
             executor.execute(new RequestHandler("request-" + i));
             Thread.sleep(200);
---
+++ src/main/java/multithread/ThreadPoolExecutorExample.java
@@ -0,0 +1,27 @@
+package multithread;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolExecutorExample {
+    public static void main(String[] args) {
+        int corePoolSize = 5;
+        int maximumPoolSize = 10;
+        int queueCapacity = 100;
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, // Số corePoolSize
+                maximumPoolSize, // số maximumPoolSize
+                10, // thời gian một thread được sống nếu không làm gì
+                TimeUnit.SECONDS, new ArrayBlockingQueue<>(queueCapacity)); // Blocking queue để cho
+                                                                            // request đợi
+        // 1000 request đến dồn dập, liền 1 phát, không nghỉ
+        for (int i = 0; i < 1000; i++) {
+            executor.execute(new RequestHandler("request-" + i));
+        }
+        executor.shutdown(); // Không cho threadpool nhận thêm nhiệm vụ nào nữa
+
+        while (!executor.isTerminated()) {
+            // Chờ xử lý hết các request còn chờ trong Queue ...
+        }
+    }
+}
