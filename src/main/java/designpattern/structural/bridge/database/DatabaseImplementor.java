f282953a12d8ef1910b21368b77cadd8a28b5cd0
Tiên Huỳnh <anhtienzz123@gmail.com>
2025-05-20 15:29:38 +0700

    Add Database example using Bridge patten

---
+++ src/main/java/designpattern/structural/bridge/database/DatabaseImplementor.java
@@ -0,0 +1,10 @@
+package designpattern.structural.bridge.database;
+
+// Implementor interface
+public interface DatabaseImplementor {
+	void connect();
+
+	void executeQuery(String query);
+
+	void disconnect();
+}
