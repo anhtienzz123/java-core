57e99abec1e96448fe8333777c079ec5d7158eec
Tiên Huỳnh <anhtienzz123@gmail.com>
2025-05-28 00:43:00 +0700

    Add Simple Arithmetic Expression Language using Visitor pattern

---
+++ src/main/java/designpattern/behavioral/interpreter/simplearithmeticexpressionlanguage/Expression.java
@@ -0,0 +1,6 @@
+package designpattern.behavioral.interpreter.simplearithmeticexpressionlanguage;
+
+// Abstract Expression: Defines the interpret method
+public interface Expression {
+	int interpret(Context context);
+}
