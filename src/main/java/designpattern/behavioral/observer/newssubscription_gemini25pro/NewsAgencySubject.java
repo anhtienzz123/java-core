87fa9918774b11657fb3b7338a64fd2f9f8f7732
Tiên Huỳnh <anhtienzz123@gmail.com>
2025-06-13 03:32:41 +0700

    Add News Subscription System using Observer pattern

---
+++ src/main/java/designpattern/behavioral/observer/newssubscription_gemini25pro/NewsAgencySubject.java
@@ -0,0 +1,8 @@
+package designpattern.behavioral.observer.newssubscription_gemini25pro;
+
+// Subject
+public interface NewsAgencySubject {
+    void subscribe(NewsSubscriberObserver subscriber);
+    void unsubscribe(NewsSubscriberObserver subscriber);
+    void notifySubscribers(String newsArticle);
+} 
